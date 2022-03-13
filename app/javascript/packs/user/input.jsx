b6e8397fb2befe80ba30d1398c3d0fdae00cdbae
dominiclai1983 <dominiclai1983@gmail.com>
2022-03-14 00:58:03 +0800

    completed add todo function

--- app/javascript/packs/user/input.jsx
+++ app/javascript/packs/user/input.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { InputGroup, Button, FormControl } from 'react-bootstrap';
+import axios from 'axios';
 
-const Input = () => {
+const Input = () =>{
+
+  const [item, setItem] = useState('');
+  
+  const handleSubmit = () => {
+    const todo = {item: item};
+
+    axios.post('api/tasks', todo)
+    .then(response => {
+      console.log(response);
+      console.log(response.data);
+      setItem('');
+    })
+  }
+
   return (
     <>
       <InputGroup className="my-2">
         <FormControl
-          placeholder="Recipient's username"
-          aria-label="Recipient's username"
+          placeholder="What is in your mind?"
+          aria-label="todo"
           aria-describedby="basic-addon"
+          value={item}
+          onChange={event =>{
+            event.preventDefault();
+            setItem(event.target.value);
+          }}
+          onSubmit={handleSubmit}
         />
-        <Button variant="warning" id="button-addon">
+        <Button variant="warning" id="button-addon" onSubmit={handleSubmit}>
           Add Task
         </Button>
       </InputGroup>
--- app/javascript/packs/user/leftnav.jsx
+++ app/javascript/packs/user/leftnav.jsx
@@ -8,7 +8,7 @@ const LeftNav = (props) => {
   return (
     <>
       <h3>Wellcome!{` ${username}`}</h3>
-      <h6 className="text-secondary"><i class="fas fa-chevron-right"></i> Your ToDo</h6>
+      <h6 className="text-secondary"><i className="fas fa-chevron-right"></i> Your ToDo</h6>
       <h6 className="text-secondary">Your Account</h6>
       <Button variant="secondary" onClick={onLogOut}>LogOut</Button>
     </>
