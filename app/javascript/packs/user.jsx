2a76afc850ec9aa7be75608faad5e3cb11ccd1d8
dominiclai1983 <dominiclai1983@gmail.com>
2022-03-16 18:57:26 +0800

    change from function component to class component

--- app/javascript/packs/user.jsx
+++ app/javascript/packs/user.jsx
@@ -1,74 +1,135 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, Component} from 'react'
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 import LeftNav from './user/leftbar';
 import Input from './user/input';
 import NavBar from './component/navbar';
 import InlineEdit from './component/inline';
 
-import { Row, Col, Container, Form, CloseButton } from 'react-bootstrap';
-import { safeCredentials, handleErrors } from './utils/fetchHelper';
+import { Row, Col, Container} from 'react-bootstrap';
+import { safeCredentials, handleErrors, checkStatus, json } from './utils/fetchHelper';
 
 import './user.scss';
 
-export default function User(){
 
-  const [todos, setTodo] = useState([]);
-  const [username, setUsername] = useState("");
-  const [authenticated, setAuth] = useState(false);
+class User extends React.Component{
 
-  const time = new Date();
+  constructor(props){
+    super(props);
+    this.state ={
+      todos: [],
+      username: "",
+      authenticated: false
+    }
+
+    this.loadAllToDo = this.loadAllToDo.bind(this);
+    this.loadActiveToDo = this.loadActiveToDo.bind(this);
+    this.loadCompletedToDo = this.loadCompletedToDo.bind(this);
+    this.updateTodo = this.updateTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.handleTodoStatus = this.handleTodoStatus.bind(this);
+
+  }
+
+  componentDidMount(){
+
+    this.checkLogin();
+
+    this.loadAllToDo(this.username);
+
+  }
+
 
   //check login method. if not login, the user will be redirect to front page
-  const checkLogin = () => {
-    axios.get('api/authenticated')
-    .then(response => {
-      console.log(response.data.authenticated);
-      setUsername(response.data.username);
-      setAuth(response.data.authenticated);
-      
-      if(!response.data.authenticated){
-        window.location.href = "/";
-      }
-      
+  checkLogin(){
+
+      fetch('api/authenticated')
+      .then(checkStatus)
+      .then(json)
+      .then(data => {
+        if(data.authenticated){
+          this.setState({
+            username: data.username,
+            authenticated: data.authenticated
+          }, () => {
+            console.log(this.state.username);
+          })
+        }else{
+          document.location.href="/";
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }
+  
+  //load all todo method
+  loadAllToDo(username){
+
+
+    fetch(`api/users/${username}/tasks`)
+    .then(checkStatus)
+    .then(json)
+    .then(data => {
+      this.setState({
+        todos: data.tasks
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+  }
+
+  //load all todo that is with status active 
+  loadActiveToDo(username){
+
+    console.log(username);
+
+    fetch(`api/users/${username}/active`)
+    .then(checkStatus)
+    .then(json)
+    .then(data => {
+      this.setState({
+        todos: data.tasks
+      })
+      console.log(data.tasks);
+    })
+
+
+  }
+
+  //load all todo that is with status completed 
+  loadCompletedToDo = (username) => {
+
+    fetch(`api/users/${username}/completed`)
+    .then(checkStatus)
+    .then(json)
+    .then(data => {
+      this.setState({
+        todos: data.tasks
+      })
+    })
+    .catch(error => {
+      console.log(error);
     });
   }
 
-  const loadAllToDo = (username) => {
-    axios.get('api/users/${username}/tasks')
-    .then(response => {
-      setTodo(response.data.tasks);
-      console.log(todos);
-      console.log(response.data.tasks)
-    })
-  }
+  handLogOut = () => {
 
-  const loadActiveToDo = (username) => {
-    axios.get('api/users/${username}/active')
-    .then(response => {
-      setTodo(response.data.tasks);
-      console.log(todos);
-    })
-  }
-
-  const loadCompletedToDo = (username) => {
-    axios.get('api/users/${username}/completed')
-    .then(response => {
-      setTodo(response.data.tasks);
-      console.log(todos);
-    })
-  }
-
-  const handLogOut = () => {
-    axios.delete('api/sessions')
-    .then(response =>{
-      if(response.data.success){
+    fetch('api/sessions', safeCredentials({
+      method: 'DELETE'
+    }))
+    .then(handleErrors)
+    .then(data =>{
+      if(data.success){
         document.location.href="/";
       }
     })
+
   }
 
-  const updateTodo = (id, msg) => {
+  updateTodo = (id, msg) => {
 
     if(!id){
       return;
@@ -86,7 +147,7 @@ export default function User(){
     })
   }
 
-  const deleteTodo = (id) => {
+  deleteTodo = (id) => {
     if(!id){
       return;
     }
@@ -99,75 +160,77 @@ export default function User(){
       console.log(res);
     })
 
-    loadAllToDo(username);
+    this.loadAllToDo(username);
 
   }
 
-  const handleTodoStatus = (id, completed) => {
+  handleTodoStatus = (id, completed) => {
     if(completed){
-    axios.put(`api/tasks/${id}/completed`)
-      .then(response => {
-        console.log(response);
+
+      fetch(`api/tasks/${id}/completed`, safeCredentials({
+        method: 'PUT'
+      }))
+      .then(handleErrors)
+      .then(res => {
+        console.log(res);
       })
+
     }else {
-      axios.put(`api/tasks/${id}/active`)
-      .then(response => {
-        console.log(response);
+
+
+      fetch(`api/tasks/${id}/active`, safeCredentials({
+        method: 'PUT'
+      }))
+      .then(handleErrors)
+      .then(res => {
+        console.log(res);
       })
     }
   }
 
-  useEffect(() => {
-    checkLogin();
-  },[]);
+  render(){
 
-  useEffect(() => {
-    loadAllToDo(username);
-  },[username]);
+    const time = new Date();
 
-  const Intro = () => {
+    const {authenticated, username, todos} = this.state;
+   
     return (
-      <h5 className="text-secondary">Let's Add Some Todo!</h5>
+      <>
+        <div className={authenticated? null : "d-none"}>
+          
+          <NavBar />
+            <Container>
+              <Row>
+                <Col xs={2} md={3} className="mt-4 text-right">
+                  <LeftNav username={username} onLogOut={this.handLogOut} />
+                </Col>
+                <Col xs={10} md={9}>
+                  <div className="right-side border">
+                    <Container>
+                      <h3 className="pt-2 trash-can">Today is {time.toLocaleDateString('en-US')}</h3>
+                      <Row>
+                        <Col xs={12}>
+                          <Input username={username} onGetAllTodo={this.loadAllToDo} />
+                        </Col>
+                        <Col xs={12} className="border-bottom mt-1 mb-1 pb-2">
+                        &emsp;<a href={null} className="kinda-link" onClick={() => {this.loadAllToDo(username)}}>All</a>&emsp;|&emsp;<a href={null} className="kinda-link" onClick={() => {this.loadActiveToDo(username)}} >Active</a>&emsp;|&emsp;<a href={null} className="kinda-link" onClick={() => {this.loadCompletedToDo(username)}} >Completed</a>
+                        </Col>
+                        <Col xs={12}>
+                        {todos.reverse().map(todo => {
+                          return <InlineEdit key={todo.id} todo={todo} onUpdate={this.updateTodo} onDelete={this.deleteTodo} onMarkCompleted={this.handleTodoStatus} />
+                        })}
+                        {(todos.length === 0 )? <h5 className="text-secondary">Let's Add Some Todo!</h5> : null}
+                        </Col>
+                      </Row>
+                    </Container>  
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+        </div>
+      </>
     )
   }
-
-   
-  return (
-    <>
-      <div className={authenticated? null : "d-none"}>
-        
-        <NavBar />
-          <Container>
-            <Row>
-              <Col xs={2} md={3} className="mt-4 text-right">
-                <LeftNav username={username} onLogOut={handLogOut} />
-              </Col>
-              <Col xs={10} md={9}>
-                <div className="right-side border">
-                  <Container>
-                    <h3 className="pt-2">Today is {time.toLocaleDateString('en-US')}</h3>
-                    <Row>
-                      <Col xs={12}>
-                        <Input username={username} onGetAllTodo={loadAllToDo} />
-                      </Col>
-                      <Col xs={12} className="border-bottom mt-1 mb-2 pb-2">
-                      &emsp;<a href={null} className="kinda-link" onClick={loadAllToDo}>All</a>&emsp;|&emsp;<a href={null} className="kinda-link" onClick={loadActiveToDo}>Active</a>&emsp;|&emsp;<a href={null} className="kinda-link" onClick={loadCompletedToDo}>Completed</a>
-                      </Col>
-                      <Col xs={12}>
-                      {todos.reverse().map(todo => {
-                        return <InlineEdit key={todo.id} todo={todo} username={username} onUpdate={updateTodo} onDelete={deleteTodo} onMarkCompleted={handleTodoStatus} />
-                      })}
-                      {(todos.length === 0 )? <Intro /> : null}
-                      </Col>
-                    </Row>
-                  </Container>  
-                </div>
-              </Col>
-            </Row>
-          </Container>
-      </div>
-    </>
-  )
 }
 
 document.addEventListener('DOMContentLoaded', () => {
